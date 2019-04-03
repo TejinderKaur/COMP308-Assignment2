76f9a2ec56835662cb9a1fc0bf0b5dab9bf1999f
TejinderKaur <kaurtejinder31095@gmail.com>
2019-04-03 17:19:22 -0400

    all pages content added with images

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-   //"URI" : "mongodb://localhost/test",
-    "URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson10b",
+   "URI" : "mongodb://localhost/tejinder",
+    // "URI" : "mongodb://tejinder:tej9395@ds064278.mlab.com:64278/comp308-w2019-lesson4a"
     "secret": "SomeSecret"
 }
